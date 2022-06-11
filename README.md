9f9fc683f378d59e828f280fd862b4472ca59d9f
JC Ketterer <jcketterer18@gmail.com>
2022-06-11 12:15:10 -0700

    added readme and schema layout

---
+++ README.md
@@ -0,0 +1,5 @@
+## Capstone 2
+
+### Basic Database Schema (Subject to Change)
+
+![](<images/QuickDBD-Free%20Diagram%20(2).png>)
---
+++ capstone_2_db_schema.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE "advocate" (
+    "id" int   NOT NULL,
+    "first_name" string   NOT NULL,
+    "last_name" string   NOT NULL,
+    "email" string   NOT NULL,
+    "password" string   NOT NULL,
+    "milestone" string   NOT NULL,
+    "start_date_of_current_milestone" date   NOT NULL,
+    "team_lead" string   NOT NULL,
+    "manager" string   NOT NULL,
+    CONSTRAINT "pk_advocate" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "skills" (
+    "advocate_id" int   NOT NULL,
+    "skill_name" string   NOT NULL
+);
+
+ALTER TABLE "skills" ADD CONSTRAINT "fk_skills_advocate_id" FOREIGN KEY("advocate_id")
+REFERENCES "advocate" ("id");
+
